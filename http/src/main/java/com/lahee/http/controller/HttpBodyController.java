3e35fed022ac4a1c28e38183b1987865b3c39af5
juhee <bora2233@naver.com>
2023-06-20 14:37:48 +0900

    feat : http body,header 이용 응답

--- http/src/main/java/com/lahee/http/controller/HttpBodyController.java
+++ http/src/main/java/com/lahee/http/controller/HttpBodyController.java
@@ -2,6 +2,9 @@ package com.lahee.http.controller;
 
 import com.lahee.http.dto.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,4 +47,30 @@ public class HttpBodyController {
         log.info("body-article-comment-complex: {}", articleComplexDto);
         return ResponseDto.from(SUCCESS);
     }
+
+    @PostMapping("/entity")
+    public ResponseEntity<ResponseDto> entity(@RequestBody ArticleDto articleDto) {
+        log.info("POST /entity : {} ", articleDto);
+        //ResponseEntity<ResponseDto> responseEntity = new ResponseEntity<>(ResponseDto.from(SUCCESS), HttpStatus.OK);
+        //커스텀 헤더 만들고 함께 응답하기
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("x-likelion-custom", "Hello Like Lion");
+        return new ResponseEntity<>(ResponseDto.from(SUCCESS), httpHeaders, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/entity-more-header")
+    public ResponseEntity<ResponseDto> entityMoreHeader(@RequestBody ArticleDto articleDto) {
+        log.info("POST /entity : {} ", articleDto);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("x-likelion-custom", "Hello Like Lion");
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("x-likelion-custom2", "it's second")
+                .headers(httpHeaders)
+                .body(ResponseDto.from(SUCCESS));
+
+    }
+
 }
