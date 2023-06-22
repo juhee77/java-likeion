93c347930f97ec28a920a0588514e4a4a1fabce2
Juhee Park <jouhi3322@gmail.com>
2023-06-22 13:08:37 +0900

    feat : 프로젝트 재구성
    feat : 프로젝트 재구성

---
+++ article-skeleton/src/main/java/com/example/article/mine/CommentDto.java
@@ -0,0 +1,18 @@
+//package com.example.article.miny;
+//
+//import com.example.article.entity.CommentEntity;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class CommentDto {
+//    private Long id;
+//    private Long articleId;
+//    private String writer;
+//    private String content;
+//
+//    public static CommentDto fromEntity(CommentEntity save) {
+//        return new CommentDto(save.getId(), save.getArticleId(), save.getWriter(), save.getContent());
+//    }
+//}
---
+++ article-skeleton/src/main/java/com/example/article/mine/CommentRepository.java
@@ -0,0 +1,10 @@
+//package com.example.article.miny;
+//
+//import com.example.article.entity.CommentEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+//    List<CommentEntity> findAllByArticleId(Long id);
+//}
