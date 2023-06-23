66c6e601b88bd4a4d0a137ec0142f918ee0230c9
Juhee Park <jouhi3322@gmail.com>
2023-06-23 14:52:15 +0900

    feat : exception 1차처리

--- contents-skeleton/src/main/java/com/example/contents/UserControllerAdvice.java
+++ contents-skeleton/src/main/java/com/example/contents/UserControllerAdvice.java
@@ -2,15 +2,19 @@ package com.example.contents;
 
 import com.example.contents.dto.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 //전역적으로 발생하는 예외를 잡아준다.
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice  // @ControllerAdvice
+// 각 Controller에 나뉘어진 ExceptionHandler 메소드를 모으는 용도
 public class UserControllerAdvice {
-    @ExceptionHandler(Exception.class)
-    public ResponseDto handleIllegalState(RuntimeException exception) {
-        return new ResponseDto("User Controller Advice 에서 처리한 예외 입니다. ");
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseDto handleIllegalState(IllegalStateException exception) {
+        ResponseDto response = new ResponseDto();
+        response.setMessage("UserControllerAdvice에서 처리한 예외입니다.");
+        return response;
     }
 }
