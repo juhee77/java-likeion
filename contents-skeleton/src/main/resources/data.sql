3416884dd9b20bfde0c04419f026df30541cd54f
Juhee Park <jouhi3322@gmail.com>
2023-06-28 16:22:26 +0900

    test : profile 환경분리를 통해 테스트 환경 분리

--- contents-skeleton/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(username, email, phone, bio)
-VALUES ('jeeho.dev', 'jeeho.dev@gmail.com', '010-1234-5678', 'Developer Developing Developers');
-
--- contents-skeleton/src/test/java/com/example/contents/UserControllerTest.java
+++ contents-skeleton/src/test/java/com/example/contents/UserControllerTest.java
@@ -16,10 +16,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
