613d5057f89d6e32574cdd6dfb7266083dd3c558
Juhee Park <jouhi3322@gmail.com>
2023-07-07 14:49:16 +0900

    feat : CustomUserDetails

--- security/src/main/java/com/lahee/security/service/JpaUseDetailManager.java
+++ security/src/main/java/com/lahee/security/service/JpaUseDetailManager.java
@@ -1,10 +1,10 @@
 package com.lahee.security.service;
 
+import com.lahee.security.entity.CustomUserDetails;
 import com.lahee.security.entity.UserEntity;
 import com.lahee.security.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,21 +22,20 @@ public class JpaUseDetailManager implements UserDetailsManager {
     public JpaUseDetailManager(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         //테스트 유저 생성
-        createUser(User.withUsername("qwer").password(passwordEncoder.encode("qwer")).build());
+//        createUser(User.withUsername("qwer").password(passwordEncoder.encode("qwer")).build());
+        createUser(CustomUserDetails.builder().username("qwer").password(passwordEncoder.encode("qwer")).email("qewr@qwer").build());
     }
 
     @Override
     // UserDetailsService.loadUserByUsername(String)
     // 실제로 Spring Security 내부에서 사용하는 반드시 구현해야 정상동작을 기대할 수 있는 메소드
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<UserEntity> optionalUser
-                = userRepository.findByUsername(username);
-        if (optionalUser.isEmpty())
+        log.info(username);
+        Optional<UserEntity> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isEmpty()) {
             throw new UsernameNotFoundException(username);
-        UserEntity userEntity = optionalUser.get();
-        return User.withUsername(userEntity.getUsername())
-                .password(userEntity.getPassword())
-                .build();
+        }
+        return CustomUserDetails.fromEntity(optionalUser.get());
     }
 
     @Override
@@ -45,15 +44,19 @@ public class JpaUseDetailManager implements UserDetailsManager {
         return this.userRepository.existsByUsername(username);
     }
 
-    @Override //새로운 사용자 저장
+    @Override
+    // 새로운 사용자를 저장하는 메소드 (선택)
     public void createUser(UserDetails user) {
-        if (this.userExists(user.getUsername())) {
+        log.info("try create user: {}", user.getUsername());
+        // 사용자가 (이미) 있으면 생성할수 없다.- 아이디 중복 상태
+        if (this.userExists(user.getUsername()))
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        try {
+            userRepository.save(((CustomUserDetails) user).newEntity());
+        } catch (ClassCastException e) {
+            log.error("failed to cast to {}", CustomUserDetails.class);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        UserEntity userEntity = new UserEntity();
-        userEntity.setUsername(user.getUsername());
-        userEntity.setPassword(user.getPassword());
-        this.userRepository.save(userEntity);
     }
 
     @Override
