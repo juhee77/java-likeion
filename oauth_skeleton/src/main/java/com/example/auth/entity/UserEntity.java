d8f2ccd6bec10213f4ffdf77576caf1c3213148e
Juhee Park <jouhi3322@gmail.com>
2023-07-11 17:24:21 +0900

    feat : oauth2 - kakao로그인 기능 추가

--- oauth_skeleton/src/main/java/com/example/auth/entity/UserEntity.java
+++ oauth_skeleton/src/main/java/com/example/auth/entity/UserEntity.java
@@ -4,18 +4,23 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "users")
 @Data
+@Table(name = "users")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // DB 제약사항 추가
+    //  DB 제약사항 추가
     @Column(nullable = false, unique = true)
     private String username;
     private String password;
 
     private String email;
     private String phone;
+
+    //  Naver, Kakao 등 소셜 로그인 제공자 문자값
+    private String provider;
+    //  Naver, Kakao 등에서 사용자를 식별하기 위해 제공한 값
+    private String providerId;
 }
