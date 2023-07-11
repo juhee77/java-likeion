c8369678a29f41ed05c836438ba47d3d5d28458f
Juhee Park <51548333+juhee77@users.noreply.github.com>
2023-07-11 17:26:53 +0900

    [Week13] day2 Oauth2 (#11)
    * feat : init project(oauth project)
    
    * feat : oauth2 - 네이버 기본 설정
    
    * feat : oauth2 - user테이블에 provider별로 저장
    
    * feat : oauth2 - kakao로그인 기능 추가

---
+++ oauth_skeleton/src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.auth.config;
+
+import com.example.auth.jwt.JwtTokenFilter;
+import com.example.auth.oauth.OAuth2SuccessHandler;
+import com.example.auth.oauth.OAuth2UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+@Configuration
+public class WebSecurityConfig {
+    private final JwtTokenFilter jwtTokenFilter;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2UserServiceImpl oAuth2UserService;
+
+    public WebSecurityConfig(JwtTokenFilter jwtTokenFilter, OAuth2SuccessHandler oAuth2SuccessHandler, OAuth2UserServiceImpl oAuth2UserService) {
+        this.jwtTokenFilter = jwtTokenFilter;
+        this.oAuth2SuccessHandler = oAuth2SuccessHandler;
+        this.oAuth2UserService = oAuth2UserService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    )
+            throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authHttp -> authHttp
+                        .requestMatchers("/token/**", "/views/**")
+                        .permitAll()
+                )
+                .oauth2Login(oauth2login -> oauth2login
+                        .loginPage("/views/login")
+                        .successHandler(oAuth2SuccessHandler)
+                        .userInfoEndpoint(userinfo -> userinfo
+                                .userService(oAuth2UserService)
+                        )
+                )
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtTokenFilter, AuthorizationFilter.class);
+
+
+        return http.build();
+    }
+}
