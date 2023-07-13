ae318707deb249e39fabaf0eab33334a21f79595
Juhee Park <jouhi3322@gmail.com>
2023-07-13 15:27:15 +0900

    feat : 헤더에 데이터를 추가하는 경우

--- websocket/stomp/src/main/java/com/example/stomp/dto/ChatMessage.java
+++ websocket/stomp/src/main/java/com/example/stomp/dto/ChatMessage.java
@@ -3,12 +3,10 @@ package com.example.stomp.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class ChatMessage {
     private Long roomId;
     private String sender;
--- websocket/stomp/src/main/java/com/example/stomp/socket/WebSocketMapping.java
+++ websocket/stomp/src/main/java/com/example/stomp/socket/WebSocketMapping.java
@@ -3,11 +3,16 @@ package com.example.stomp.socket;
 import com.example.stomp.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Controller
@@ -17,9 +22,10 @@ public class WebSocketMapping {
 
     @MessageMapping("/chat")
     //pub 와 sub 패턴
-    public void sendChat(ChatMessage chatMessage) {
+    public void sendChat(ChatMessage chatMessage, @Headers Map<String, Object> headers) {
         log.info(chatMessage.toString());
-        String time = new SimpleDateFormat("HH:mm").toString();
+        log.info(headers.toString()); //헤더도 이렇게 확인이 가능하다.
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
         chatMessage.setTime(time);
         simpMessagingTemplate.convertAndSend(
                 String.format("/topic/%s", chatMessage.getRoomId()),
