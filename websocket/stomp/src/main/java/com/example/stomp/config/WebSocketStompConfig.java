ba5761b0262c7bf5eabdb2b9a91054e211f01090
Juhee Park <jouhi3322@gmail.com>
2023-07-13 17:31:49 +0900

    feat : 입장시 입장 확인 메세지를 확인할 수 있게 하는법

--- websocket/stomp/src/main/java/com/example/stomp/config/WebSocketStompConfig.java
+++ websocket/stomp/src/main/java/com/example/stomp/config/WebSocketStompConfig.java
@@ -20,7 +20,11 @@ public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
     //메세지 브로커를 활용하는 방법 설정
     //어디로 메세지를 연결할건지
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메세지 브로커를 활성화, 구독자 주소의 접두어로 해당 주소를 구독한 클라이언트에게 메시지를 전달할 수 있도록한다.
+        // topic으로 시작하는 주소로 구독한 클라이언트에게 서버에서 메시지를 전송할 수 있다.
         registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+        // 애플리케이션의 목적지 접두사 메시지 발행 주소로 나타내어
+        // app,topic으로 시작하는 주소를 클라이언트의 발행 주소로 설정하여 클라이언트가 서버로 메세지를 발행할 수 있도록한다.
+        registry.setApplicationDestinationPrefixes("/topic","/app");
     }
 }
--- websocket/stomp/src/main/java/com/example/stomp/socket/WebSocketMapping.java
+++ websocket/stomp/src/main/java/com/example/stomp/socket/WebSocketMapping.java
@@ -3,16 +3,17 @@ package com.example.stomp.socket;
 import com.example.stomp.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
 @Controller
@@ -23,7 +24,7 @@ public class WebSocketMapping {
     @MessageMapping("/chat")
     //pub 와 sub 패턴
     public void sendChat(ChatMessage chatMessage, @Headers Map<String, Object> headers,
-                         @Header("nativeHeaders") Map<String,String> nativeHeaders) {
+                         @Header("nativeHeaders") Map<String, String> nativeHeaders) {
         log.info(chatMessage.toString()); //ChatMessage(roomId=1, sender=lion, message=123, time=null)
         log.info(headers.toString()); //헤더도 이렇게 확인이 가능하다. //{simpMessageType=MESSAGE, stompCommand=SEND, nativeHeaders={Authorization=[Bearer Token_here], destination=[/app/chat], content-length=[44]}, simpSessionAttributes={}, simpHeartbeat=[J@6cbea075, lookupDestination=/chat, simpSessionId=59e45c4d-e57e-d83e-750e-136e698df78a, simpDestination=/app/chat}
         log.info(nativeHeaders.toString()); // 우리가 넣은것만 확인할 수도 있다. //{Authorization=[Bearer Token_here], destination=[/app/chat], content-length=[44]}
@@ -34,4 +35,18 @@ public class WebSocketMapping {
                 chatMessage
         );
     }
+
+    //누군가가 구독할때 실행하는 메소드
+    @SubscribeMapping("/{roomId}")//post매핑과 get매핑 같은 차이
+    public ChatMessage sentGreet(@DestinationVariable("roomId") Long roomId) {
+        log.info("new subscription to {} ", roomId);
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        ChatMessage chatMessage = new ChatMessage(roomId, "ADMIN", "환영!!", time);
+        return chatMessage;
+    }
+
+
+    //전체에게 보내는 예시
+
+
 }
