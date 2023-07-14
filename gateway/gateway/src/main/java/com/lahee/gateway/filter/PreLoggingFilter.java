a2f28767a52271c3350cfb3bd2123afb13cc1441
Juhee Park <jouhi3322@gmail.com>
2023-07-14 14:19:41 +0900

    feat : gateway preloggingFilter - 헤더 추가

---
+++ gateway/gateway/src/main/java/com/lahee/gateway/filter/PreLoggingFilter.java
@@ -0,0 +1,47 @@
+package com.lahee.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.UUID;
+@Component
+@Slf4j
+public class PreLoggingFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(
+            // HttpServletRequest & Response 대신
+            // exchange
+            ServerWebExchange exchange,
+            // FilterChain
+            GatewayFilterChain chain
+    ) {
+        //사용자가 보낸 HTTP 요청을 받았다.
+        ServerHttpRequest httpRequest = exchange.getRequest();
+        // PreLoggingFilter에서 요청을 식별할 수 있는 HTTP Header를 작성
+        // 나중에 PostLoggingFilter에서 해당 Header을 바탕으로
+        // 실행에 걸린 시간 측정
+
+        //사용자가 보낸 요청을 조작(변형) 하겠다.
+        String requestId = UUID.randomUUID().toString();
+        httpRequest.mutate()
+                //헤더를 변형한다.
+                .headers(httpHeaders -> {
+                    httpHeaders.add(
+                            "x-gateway-request-id",
+                            requestId
+                    );
+                    httpHeaders.add(
+                            "x-gatway-request-time",
+                            String.valueOf(Instant.now().toEpochMilli())
+                    );
+                });
+        log.info("start transaction: {}", requestId);
+        return chain.filter(exchange);
+    }
+}
